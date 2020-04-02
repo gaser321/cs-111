b4ab763c6b8673861de9c1a26277e1a59b852d0b
gaser321 <61541888+gaser321@users.noreply.github.com>
2020-04-02 15:02:24 +0200

    Add files via upload

---
+++ WebSite1/dbo.Table.sql
@@ -0,0 +1,14 @@
+﻿CREATE TABLE Student__
+
+(
+	[ID] INT NULL PRIMARY KEY, 
+    [Fname] NVARCHAR(50) NOT NULL, 
+    [Lname] NVARCHAR(50) NOT NULL, 
+    [Gendar] NVARCHAR(50) NOT NULL, 
+    [BirthDate] DATE NOT NULL, 
+    [Email] NVARCHAR(50) NOT NULL, 
+    [Bus] NVARCHAR(50) NOT NULL, 
+    [Faculty] NVARCHAR(50) NOT NULL, 
+    [Address] NVARCHAR(50) NOT NULL, 
+    [Password] NVARCHAR(50) NOT NULL
+)
